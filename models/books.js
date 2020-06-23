ccd085d283a07ab6eb14ed3d68161e1afb896c97
Guillermo Lara <grlara9@gmail.com>
2020-06-23 16:34:35 -0700

    work on books schema

---
+++ models/books.js
@@ -0,0 +1,13 @@
+const moongoose= require('mongoose');
+const Schema = moongoose.Schema;
+
+const BookSchema = new Schema({
+title:{type:String, },
+authors:{type:String},
+description:{type:String},
+image: {type: String},
+link:{type:String}
+})
+
+const Book = moongoose.model('Book', BookSchema);
+module.exports = Book
