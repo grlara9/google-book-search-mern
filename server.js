905d17a71a8dca97ce4e4e7f3b861c1b5925bac6
Guillermo Lara <grlara9@gmail.com>
2020-06-23 17:19:15 -0700

    connect to mongodb successfully established

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ var PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log("MongoDB database connection established successfully");
+    
+})
+
 app.listen(PORT, ()=>{
     console.log("Listening on PORT: " + PORT);
 })
