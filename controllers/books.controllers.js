7ebe22b2ae6bb29168e82e5166d4e381265bc4a6
Guillermo Lara <grlara9@gmail.com>
2020-06-25 21:21:29 -0700

    created get route to get all save books in database

--- controllers/books.controllers.js
+++ controllers/books.controllers.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 var Book = require("../models/books");
 
+router.get("/books", (req, res)=>{
+    Book.find()
+    .then(promise => res.json(promise))
+    .catch(err=> res.status(400).json("Error: "+ err))
+})
+
 router.post("/books", (req, res)=>{
     const {title, authors, description, image, link} = req.body
     const newBook = new Book({
