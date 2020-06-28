fe71d33ef0e7b939ead8e6065e2ac0e8b976b422
Guillermo Lara <grlara9@gmail.com>
2020-06-27 19:20:04 -0700

    made axios request to google book api

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
+class App extends React.Component() {
+  constructor(props){
+    super(props);
+      this.state={
+        books:[],
+        query:'java'
+      }
+  }
+
+  getBook = (e) =>{
+    e.preventDefault();
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + this.state.query)
+    .then(response =>{
+      console.log(response)
+     // this.setState({ books: [...data.data.items ] })
+    })
+    .catch(error =>console.log("Error: " + error))
+  }
+
+  render(){
   return (
     <div className="App">
-     <h1>hello world</h1>
+     <h1>g</h1>
     </div>
   );
 }
-
+}
 export default App;
