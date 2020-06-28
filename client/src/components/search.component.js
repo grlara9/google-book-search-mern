ea93d89834636d7e536465a425244fe24a585181
Guillermo Lara <grlara9@gmail.com>
2020-06-28 16:13:29 -0700

    work on form to search for books

--- client/src/components/search.component.js
+++ client/src/components/search.component.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 
 
- class Search extends Component {
-    render() {
-        return (
-            <div>
-                <h1>this is search</h1>
+ const Search = ()=> {
+    return (
+        <form onSubmit={(e) => {props.onSubmit(e)}}> 
+            <div className="row">
+
+            <div className="form-group col-md-8">
+
+            <input  type="text" 
+            name="book"
+            onChange={(e)=> {props.onChange(e.target.value)}}
+            className="form-control form-control-lg" 
+            placeholder="Search for Book..."/>
             </div>
+            <div className="form-group col-md-4">
+
+            <button type="submit" className="btn btn-primary btn-lg btn-block">Search..</button>
+            </div>
+            </div>
+        </form>
+        
         )
-    }
+    
 }
 export default Search
