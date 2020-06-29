b4972fd575782df87462e7562bcff223faf56f29
Guillermo Lara <grlara9@gmail.com>
2020-06-28 21:15:20 -0700

    finish booklist component and displays results

--- client/src/components/booklist.component.js
+++ client/src/components/booklist.component.js
@@ -3,7 +3,16 @@ import React from 'react'
 
 
 function BookList(props) {
-
-    
+    return( 
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="card">
+                <img src={props.image} alt="text" className="card-img-top" />
+                  <p className="card-text">Title: {props.title}</p>
+                  <p className="card-text">Author: {props.author}</p>
+                  <p className="card-text">Published Date: {props.published}</p>
+                  <button type="button" className="btn btn-info"><a href={props.preview}>View</a></button>
+            </div>
+        </div>
+    )
 }
 export default BookList
