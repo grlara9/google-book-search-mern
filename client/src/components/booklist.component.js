349a74dd19f0c1177e3c332878f6cad849619286
Guillermo Lara <grlara9@gmail.com>
2020-07-14 21:36:54 -0700

    created rows and columns on booklist component

--- client/src/components/booklist.component.js
+++ client/src/components/booklist.component.js
@@ -6,24 +6,36 @@ function BookList(props)  {
 
   return( 
        
-            <div className="book-container">
+            <div className="card">
                 <div className="row">
-                    <div className="title-author">
-                        <p className="card-text">Title: {props.title}</p>
-                        <p className="card-text">Author: {props.author}</p>
+                    <div className="column">
+                        <div className="header">
+                            <p className="card-text">Title: {props.title}</p>
+                            <p className="card-text">Author: {props.author}</p>
+                        </div>
                     </div>
-                    <div className="buttons-div">
+                    <div className="column">
                         <button type="button" className="btn btn-info"><a href={props.preview}>View</a></button>
                         <button className="saveBook btn btn-primary" id={props.key} onClick={(event) => props.handleSavedButton(event)}>Save Book</button>
                     </div>
-               </div>
+                  </div>  
+                  
+            <div className="row">
+                <div className="column">
+                    <div className="photo">
+                        <img src={props.image}  className="img" alt="text"  />
+                    </div>
+                </div>
 
-               <div className="row">
-                    <img src={props.image}  alt="text" className="card-img-top" />
-                    <p className="card-text">Published Date: {props.published}</p>
-                    <p className="card-text">Description: {props.description}</p>
-               </div>
+                <div className="column">
+                    <div className="content">
+                        <p className="card-text">Published Date: {props.published}</p>
+                        <p className="card-text">Description: {props.description}</p>
+                    </div>
+                </div>
             </div>
+                </div>                    
+            
        
     )
 
