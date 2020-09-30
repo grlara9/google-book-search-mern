abc9f59dd1ddeaeebee3082ba469999527577edb
Guillermo Lara <grlara9@gmail.com>
2020-09-29 17:52:45 -0700

    create new folder shared to store all components for new navigtion

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var PORT = process.env.PORT || 5000;
  
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
