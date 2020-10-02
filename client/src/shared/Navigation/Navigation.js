86c2353f78ba72a27daffb5eebfaea9dc030180b
Guillermo Lara <grlara9@gmail.com>
2020-10-01 22:36:42 -0700

    created two new js files on Navigation directory

--- client/src/shared/Navigation/Navigation.js
+++ client/src/shared/Navigation/Navigation.js
@@ -4,18 +4,33 @@ import { Link } from 'react-router-dom';
 import Header from './Header';
 import SideDrawer from './SideBar'
 import NavLinks from './NavLinks'
-
+import Backdrop from './Backdrop'
 import './Navigation.css';
 
 const Navigation = props => {
-    return (
-  <React.Fragment>
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+          <nav className="main-navigation__drawer-nav"><NavLinks /></nav>
+        </SideDrawer>
+
       <Header>
-        <button className="main-navigation__menu-btn" >
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
           <span />
           <span />
           <span />
-        </button>
+        </button> 
         <h1 className="main-navigation__title">
           <Link to="/">YourPlaces</Link>
         </h1>
@@ -26,5 +41,6 @@ const Navigation = props => {
     </React.Fragment>
   );
 };
+
 
 export default Navigation;
